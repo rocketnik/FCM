8d12f41403d871f4ffcebd26c8b02be3b6550a76
Chinh Tran <ct@contexon.com>
2023-11-09 14:30:48 +0100

    Add get topics request for a given token

---
+++ Sources/FCM/Helpers/FCM+GetTopics.swift
@@ -0,0 +1,29 @@
+import Foundation
+import Vapor
+
+extension FCM {
+    public func getTopics(token: String) async throws -> [String] {
+        return try await _getTopics(token: token)
+    }
+
+    private func _getTopics(token: String) async throws -> [String] {
+        let headers = makeHeaders()
+        let url = self.iidURL + "info/\(token)?details=true"
+        let response = try await client.get(URI(string: url), headers: headers).validate()
+
+        let result = try response.content.decode(TopicsResponse.self, using: JSONDecoder())
+        return Array(result.rel.topics.keys)
+    }
+}
+
+struct TopicsResponse: Codable {
+    let rel: SubscribedTopics
+
+    struct SubscribedTopics: Codable {
+        let topics: [String: Topic]
+    }
+
+    struct Topic: Codable {
+        let addDate: String
+    }
+}
